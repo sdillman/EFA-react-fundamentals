db7b2bc94ec21ea742f0fb1eab1cf90db81eb537
Shannon D <sdillman@users.noreply.github.com>
2021-06-22 18:41:48 -0400

    continuing to enter code. nothing to see yet

---
+++ src/components/site/Footer.js
@@ -0,0 +1,13 @@
+import {row} from 'reactstrap';
+
+const Footer = () => {
+    return(
+        <footer>
+            <row>
+                <p>&copy Eleven Fifty 2021</p>
+            </row>
+        </footer>
+    );
+};
+
+export default Footer;
