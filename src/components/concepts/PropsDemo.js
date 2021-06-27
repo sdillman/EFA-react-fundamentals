97c1d6f64cea171b601135229cb889bffd87ec11
Shannon D <sdillman@users.noreply.github.com>
2021-06-27 11:13:42 -0400

    Through 6.5, passing functions via props

--- src/components/concepts/PropsDemo.js
+++ src/components/concepts/PropsDemo.js
@@ -4,13 +4,50 @@ import { useState } from 'react';
 const PropsDemo = () => {
 // Passes a prop of 'string' to a child functional component
 // HAS the string to display, but calls (and depends on) the child component "FunctionalComponent" to do the work
+
+const [ color, setColor ] = useState('white');
+const [ backgroundColor, setBackgroundColor ] = useState('purple');
+const [ borderRadius, setBorderRadius ] = useState('5px');
+const [ borderStyle, setBorderStyle ] = useState('dashed');
+const [ display, setDisplay ] = useState('inline-block');
+const [ width, setWidth ] = useState('350px');
+const [ textAlign, setTextAlign ] = useState('center');
+
+let styles = {
+    color : color,
+    backgroundColor : backgroundColor,
+    borderRadius : borderRadius,
+    borderStyle : borderStyle,
+    display : display,
+    width : width,
+    textAlign : textAlign
+    };
+
+const toggleColor = () => {
+    color === 'white' ? setColor('pink') : setColor('white');
+}
+const toggleBkgndColor = () => {
+    backgroundColor === 'purple' ? setBackgroundColor('#333333') : setBackgroundColor('purple');
+}
+const toggleBorderRadius = () => {
+    borderRadius === '5px' ? setBorderRadius('50px') : setBorderRadius('5px');
+}
+const toggleBorderStyle = () => {
+    borderStyle === 'dashed' ? setBorderStyle('solid') : setBorderStyle('dashed');
+}
+
+
     return (
         <div className='main'>
             <div className='mainDiv'>
-                <FunctionalComponent string='will this display?' />
-                <FunctionalComponent string='it should' />
-                <FunctionalComponent string='wakey wakey FunctionalComponent' />
-                <FunctionalComponent string='your job to get this out there!' />
+                <div style={styles}>
+                    <FunctionalComponent string="Wanna toggle the text color?" function={toggleColor} />
+                    <FunctionalComponent string="How 'bout the background?" function={toggleBkgndColor} />
+                    <FunctionalComponent string='Border radius?' function={toggleBorderRadius} />
+                    <FunctionalComponent string='your job to get this out there!' function={toggleBorderStyle} />
+
+                </div>
+                
             </div>
         </div>
     );
@@ -19,11 +56,13 @@ const PropsDemo = () => {
 
 export default PropsDemo;
 
+
 const FunctionalComponent = (props) => {
     // This is the child functional component
     return(
         <div>
             <p>{props.string}</p>
+            <button onClick={props.function}>Press Me!</button>
         </div>
     )
 }
