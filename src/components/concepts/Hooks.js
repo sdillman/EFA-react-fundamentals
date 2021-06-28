7517aecf852c243bf7cc323680e6dd0ad9b38bb2
Shannon D <sdillman@users.noreply.github.com>
2021-06-28 12:28:41 -0400

    Through 7.2

---
+++ src/components/concepts/Hooks.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+
+const Hooks = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState({});
+
+// function to fetch the associated Star Wars character from the Star Wars API
+  const fetcher = () => {
+      fetch(`https://swapi.dev/api/people/${query}`)
+      .then(res => res.json())
+      .then(json => {
+          console.log(json)
+          setResults(json);
+      })
+  }
+
+return(
+  <div className="main">
+   <div className="mainDiv">
+    <input value={query} onChange={e => setQuery(e.target.value)} placeholder="enter your sw character number"/>
+    <button onClick = {() => fetcher()}>Click for Character!</button>
+    {/* See a result - 'results' comes from the useState above */}
+    {results ? <h2>{results.name}</h2> : <div></div>}
+   </div>
+  </div>
+ )
+}
+
+export default Hooks;
--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -11,6 +11,7 @@ import JSXRules from '../concepts/JSXRules';
 import State from '../concepts/State';
 import Effects from '../concepts/Effects';
 import PropsDemo from '../concepts/PropsDemo';
+import Hooks from '../concepts/Hooks';
 
 const Sidebar = () => {
     return(
@@ -24,6 +25,7 @@ const Sidebar = () => {
                     <li><Link to='/effects'>useEffects</Link></li>
                     <li><Link to='/resources'>Resources</Link></li>
                     <li><Link to='/propsdemo'>PropsDemo</Link></li>
+                    <li><Link to='/hooks'>Hooks</Link></li>
                 </ul>
             </div>
             <div className='sidebar-route'>
@@ -34,8 +36,9 @@ const Sidebar = () => {
                     <Route exact path='/jsxrules'><JSXRules /></Route>
                     <Route exact path='/state'><State /></Route>
                     <Route exact path='/effects'><Effects /></Route>
-                    <Route exact path='/resources'><Resources /></Route>
                     <Route exact path='/propsdemo'><PropsDemo /></Route>
+                    <Route exact path='/hooks'><Hooks /></Route>
+                    <Route exact path='/resources'><Resources /></Route>
                 </Switch>
             </div>
         </div>
