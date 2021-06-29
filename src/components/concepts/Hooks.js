8691a446664c43857dc7c28c5e9f5af03ff68abe
Shannon D <sdillman@users.noreply.github.com>
2021-06-29 01:54:45 -0400

    Through 7.4

--- src/components/concepts/Hooks.js
+++ src/components/concepts/Hooks.js
@@ -14,16 +14,65 @@ const Hooks = () => {
       })
   }
 
-return(
-  <div className="main">
-   <div className="mainDiv">
-    <input value={query} onChange={e => setQuery(e.target.value)} placeholder="enter your sw character number"/>
-    <button onClick = {() => fetcher()}>Click for Character!</button>
-    {/* See a result - 'results' comes from the useState above */}
-    {results ? <h2>{results.name}</h2> : <div></div>}
-   </div>
-  </div>
- )
+  return(
+    <div className="main">
+    <div className="mainDiv">
+      <input value={query} onChange={e => setQuery(e.target.value)} placeholder="enter your sw character number"/>
+      <button onClick = {() => fetcher()}>Click for Character!</button>
+      {/* See a result - 'results' comes from the useState above */}
+      {results ? <h2>{results.name}</h2> : <div></div>}
+    </div>
+    </div>
+  )
 }
 
-export default Hooks;
+
+const Hooks2 = () => {
+  const [results, queryNum, setQueryNum] = useNumHook('')
+  
+  const [clicks, setClicks] = useClicks(0);
+
+  return(
+   <div className="main">
+    <div className="mainDiv">
+     <h3>Enter a number below to see a number fact</h3>
+     <input value={queryNum} onChange={e => setQueryNum(e.target.value)} placeholder="enter a number" />
+     {results ? <h2>{results}</h2> : <div></div>}
+
+     <button onClick={() => setClicks(clicks + 1)}>Click to update document title</button>
+
+    </div>
+   </div>
+  )
+ }
+
+
+const useNumHook = (num) => {
+  const [queryNum, setQueryNum] = useState(num);
+  const [results, setResults] = useState('');
+
+  useEffect(() => {
+    // using an Effect because a fetch is a side-effect - does not directly affect the DOM
+    if (queryNum !== ''){
+      fetch(`http://numbersapi.com/${queryNum}`)
+        .then(res => res.text())
+        .then(json => {
+          setResults(json);
+          console.log(json)
+         });
+      };
+   }, [queryNum]);
+  return [results, queryNum, setQueryNum];
+};
+
+/* ---------------------- */
+const useClicks = (initCount) => {
+   const [clicks, setClicks] = useState(initCount);
+  
+   useEffect(() => {
+    document.title = `You have clicked ${clicks} times`;
+   }, [clicks])
+   return [clicks, setClicks]
+}
+
+export default Hooks2;
