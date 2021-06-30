f70915b087db25c8eda43bb035f7172d39683217
Shannon D <sdillman@users.noreply.github.com>
2021-06-29 21:30:40 -0400

    8.4, didn't hafta look! But this time _completely_ done

--- src/components/apps/timer-apps/ClockApp.js
+++ src/components/apps/timer-apps/ClockApp.js
@@ -8,6 +8,14 @@ const getTimeString = () => {
 const ClockApp = () => {
 
     const [time, setTime] = useState(getTimeString());
+
+    useEffect( () => {
+        const interval = setInterval(() => {
+            let date = getTimeString();
+            setTime(date);
+        })
+    }, 1000);
+
     return (
         <div>
             <h1 className = "section-title">React Clock</h1>
