2e4acc70389dd9febf7c3f9cecf03ef5a8f876c1
Shannon D <sdillman@users.noreply.github.com>
2021-08-22 00:14:42 -0400

    1.02 CLASS COMPONENT CHALLENGE - done, added a Net Likeability number the increments and decrements when Likes/Dislikes are changed, and included a colorControl() function to change that number to red when < 0

--- src/components/concepts/ClassComponentDemo.js
+++ src/components/concepts/ClassComponentDemo.js
@@ -10,28 +10,58 @@ export default class ClassComponentDemo extends Component {
         super(props);
         // 'this.state' to create the initial state of this component
         // example - this.state = { simpleMessage: "Welcome user!!!" };
-        this.state = { count: 0 };
+        this.state = { likesCount: 0, dislikesCount: 0, likeability: 0};
     }
 
     incrementCount = () => {
         console.log("increment called");
         this.setState(
-            { count: this.state.count + 1 }
+            { likesCount: this.state.likesCount + 1, 
+                likeability: this.state.likeability + 1 }
         );
     };
+    decrementCount = () => {
+        console.log("decrement called");
+        this.setState(
+            { dislikesCount: this.state.dislikesCount - 1,
+                likeability: this.state.likeability - 1 }
+        );
+    };
+    
     // in all class components in React, a render method is required. This is the JSX that gets packaged up, exported, and rendered when our component gets called.
     // It's important to note and remind you here that we can create another component (Class or Functional) and call it, as we do below with <ClassComponentNotes />
     render() {
         console.log("render called");
+        let likeabilityColor = {
+            color: 'black'
+        };
+
+        // declared a function to base the style of the Net likeability number off of its value
+        const colorControl = function(likeability) {
+            return (
+        likeability < 0 ? likeabilityColor = { color: 'red' } : likeabilityColor);
+        };
+        // then called that function
+        colorControl(this.state.likeability);
+
+    
         return (
             <div className = "main">
                 <div className = "mainDiv">
                 <ClassComponentNotes />
                 <hr />
-                    <h3>Smash that like button!</h3>
+                    <h3>Smash the best button for you!</h3>
                     <button onClick = { this.incrementCount }>
-                        Likes: { this.state.count }
+                        Likes: { this.state.likesCount }
                     </button>
+
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+
+                    <button style= {{color: "red"}} onClick = { this.decrementCount }>
+                        DISlikes: { this.state.dislikesCount }
+                    </button>
+
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Net likeability: </span><span style= { likeabilityColor }>{ this.state.likeability }</span>
                 </div>
             </div>
         );
