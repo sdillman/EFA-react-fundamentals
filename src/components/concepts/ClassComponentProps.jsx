d1b859d6a4d27b0f462e32557433f635d6018ddb
Shannon D <sdillman@users.noreply.github.com>
2021-08-24 00:51:28 -0400

    informed by 1.10 CLASSCOMPONENTPROPS.JSX FINAL CODE

--- src/components/concepts/ClassComponentProps.jsx
+++ src/components/concepts/ClassComponentProps.jsx
@@ -15,35 +15,40 @@ export default class ClassComponentProps extends Component {
 
 
     // The first method should update the newFruit property in the state when the value of the input changes. Listening for e...
-    updateNewFruit = (e) => {
-        console.log("updating the newFruit property in the state when the value of the input changes.");
-        this.setState(
-            // never used (e), but the correct answer does. Study
-            { newFruit: this.state.newFruit }
-        );
-    };
+
+    // my incorrect answer, to study:
+    // updateNewFruit = (e) => {
+    //     console.log("updating the newFruit property in the state when the value of the input changes.");
+    //     this.setState(
+    //         // never used (e), but the correct answer does. Study
+    //         { newFruit: this.state.newFruit }
+    //     );
+    // };
+
     // correct answer, to compare
-    // changeHandler(event) {
-    //     this.setState({ newFruit: event.target.value });
-    //     }
+    changeHandler(event) {
+        this.setState({ newFruit: event.target.value });
+        }
 
     // The second method should add the value of newFruit to the array of fruits and reset the value of the input field to be empty.
-    addTheNewFruit = () => {
-        console.log("adding the value of newFruit to the array of fruits");
-        this.setState({ 
-            fruit: this.state.fruit.push(this.state.newFruit),
-            newFruit: ""
-        });
-        // correct answer, to compare:
-        //     addFruit(event) {
-        //         event.preventDefault();
-        //         this.setState({
-        //             fruitBowl: [...this.state.fruitBowl, this.state.newFruit],
-        //             newFruit: "",
-        //         });
-        //     }
 
-    };
+    // MY INCORRECT ANSWER BELOW
+    // addTheNewFruit = () => {
+    //     console.log("adding the value of newFruit to the array of fruits");
+    //     this.setState({ 
+    //         fruit: this.state.fruit.push(this.state.newFruit),
+    //         newFruit: ""
+    //     });
+    
+        // correct answer, to compare:
+            addFruit(event) {
+                event.preventDefault();
+                this.setState({
+                    fruitBowl: [...this.state.fruitBowl, this.state.newFruit],
+                    newFruit: "",
+                });
+            }
+
     
 
     render() {
@@ -53,20 +58,43 @@ export default class ClassComponentProps extends Component {
             <div className = "main">
                 <div className = "mainDiv">
                 
-                <form onSubmit={this.addTheNewFruit}>
+                {/* <form onSubmit={this.addTheNewFruit}> */}
+                <form onSubmit={this.addFruit}>
 
-                <h3>Enter a new fruit</h3>
-                <div class="field">
-                    <label for="fruitName">Fruit:</label>
-                    <input type="text" id="fruitName" name="fruitName" value={this.state.newFruit} onChange = { (e) => this.updateNewFruit(e) }/>
+                    <h3>Enter a new fruit</h3>
+                    <div class="field">
+                        <label for="fruitName">Fruit:</label>
+                        <input type="text" id="fruitName" name="fruitName" value={this.state.newFruit} onChange = { (e) => this.changeHandler(e) }/>
 
-                    <button type="submit">Fruit Me!</button>
-                </div>
+                        <button type="submit">Fruit Me!</button>
+                    </div>
                 </form>
+
+                <FruitBowl fruits={ this.state.fruitBowl } />
                     
                 </div>
 
             </div>
         )
     };
+};
+
+class FruitBowl extends Component {
+    render () {
+        return (
+        <div>
+            { this.props.fruits.map((fruit) => {
+                return <Fruit passingFruit={fruit} />
+            })}
+        </div>
+        );
+    }
 }
+
+const Fruit = (props) => {
+    return (
+        <div>
+            <h3> {props.passingFruit} </h3>
+        </div>
+    );
+};
